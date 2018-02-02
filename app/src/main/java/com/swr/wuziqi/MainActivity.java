339fe0cdf37d7788b83bf0684abfd48ea4a71ea6
swr <t434343435@163.com>
2018-02-02 21:17:33 +0800

    initialize
    Signed-off-by: swr <t434343435@163.com>

--- app/src/main/java/com/swr/wuziqi/MainActivity.java
+++ app/src/main/java/com/swr/wuziqi/MainActivity.java
@@ -1,7 +1,11 @@
 package com.swr.wuziqi;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MainActivity Cont =  this;
+        final Button button1 = (Button) findViewById(R.id.button_scan);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent serverIntent = new Intent((Context) Cont, DeviceListActivity.class);
+                startActivityForResult(serverIntent, 1);
+            }
+        });
+        final Button button2 = (Button) findViewById(R.id.button_exit);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
